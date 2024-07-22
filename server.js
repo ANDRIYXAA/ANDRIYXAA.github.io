01daf5d7883d1976521287d558fe0868d6dcdcef
ANDRIYXAA <97945684+ANDRIYXAA@users.noreply.github.com>
2024-07-23 00:04:05 +0300

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
+const orders = [];
+
+app.post('/orders', (req, res) => {
+    const order = req.body;
+    orders.push(order);
+    res.status(201).json(order);
+});
+
+app.get('/orders', (req, res) => {
+    res.json(orders);
+});
 
 io.on('connection', (socket) => {
     console.log('A user connected');
@@ -24,20 +37,4 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-
-// server.js (додатковий код)
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-const orders = [];
-
-app.post('/orders', (req, res) => {
-    const order = req.body;
-    orders.push(order);
-    res.status(201).json(order);
-});
-
-app.get('/orders', (req, res) => {
-    res.json(orders);
 });
